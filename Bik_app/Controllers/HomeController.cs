0dce450132cc428b98b585fc8df88fc6af42b00c
furkan.gencal <furkan.gencal@GMNB-FGENCAL>
2022-02-04 18:51:40 +0300

    04.02.2022

--- Bik_app/Controllers/HomeController.cs
+++ Bik_app/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Bik_app.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IRepository _repository;
         //Dependency injection
-        public HomeController(ILogger<HomeController> logger,IRepository repository, AdventureWorks2017Context _adventureWorks2017Context)
+        public HomeController(ILogger<HomeController> logger, IRepository repository, AdventureWorks2017Context _adventureWorks2017Context)
         {
             _logger = logger;
             _repository = repository;
@@ -36,14 +36,6 @@ namespace Bik_app.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult AddProduct(Product product)
-        //{
-            //this.Context.Add(product);
-            //this.Context.SaveChanges();
-            //return View(product);
-        //}
-
         public JsonResult AddProductForm(Product product)
         {
             this.Context.Add(product);
@@ -51,29 +43,20 @@ namespace Bik_app.Controllers
             return Json(product);
         }
 
+
+
         public IActionResult EditProduct(int id)
         {
             return View(Context.Products.Where(p => p.ProductId == id).FirstOrDefault());
         }
 
         [HttpPost]
-        public IActionResult EditProduct(Product product)
+        public IActionResult EditTheProduct(Product product)
         {
             Context.Products.Update(product);
             Context.SaveChanges();
             return RedirectToAction("Index");
         }
-
-        //[HttpPost]
-        //public IActionResult DeleteProduct(int id)
-        //{
-        //    var product = Context.Products.Where(a => a.ProductId == id).FirstOrDefault();
-        //    Context.Products.Remove(product);
-        //    Context.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-
         public JsonResult DeleteProduct(int id)
         {
             var product = Context.Products.Where(a => a.ProductId == id).FirstOrDefault();
@@ -94,3 +77,19 @@ namespace Bik_app.Controllers
         }
     }
 }
+//[HttpPost]
+//public IActionResult AddProduct(Product product)
+//{
+//this.Context.Add(product);
+//this.Context.SaveChanges();
+//return View(product);
+//}
+
+//[HttpPost]
+//public IActionResult DeleteProduct(int id)
+//{
+//    var product = Context.Products.Where(a => a.ProductId == id).FirstOrDefault();
+//    Context.Products.Remove(product);
+//    Context.SaveChanges();
+//    return RedirectToAction("Index");
+//}
